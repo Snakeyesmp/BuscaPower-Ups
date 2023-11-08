a2c92274784125a5967bd4f96d5952af7480918d
Snakeyesmp <snakeyes271101@gmail.com>
2023-11-08 10:54:22 +0100

    Grid empezado

--- app/src/main/java/com/example/buscaminas/MainActivity.kt
+++ app/src/main/java/com/example/buscaminas/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.buscaminas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // se crea un objeto toolbar, ¡importante importar el correcto!
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        // hace que la toolbar funcione como actionbar para la activity window actual
+        setSupportActionBar(toolbar)
+
+
+
     }
+
+    /**
+     *Metodo que se usa para inflar un menu dentro de la toolbar y que aparezca
+     */
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.menu,menu)
+        return true
+    }
+
 }
